ff0332efadb59ab372552ae4d958751938bee6c8
CaoTuanKiett <tuankiet06032002@gmail.com>
2023-01-13 10:10:26 +0700

    'update_4'

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+
+
+const options = document.querySelectorAll('.people__right--item')
+// const onProfile = document.querySelector(".right__text--click");
+const onProfile = document.querySelector(".right__text--click");
+
+// function profileClick() {
+//     onProfile.classList.toggle("right__text--primary");
+// }
+
+console.log(onProfile);
+
+
+// options.forEach(option => {
+//     option.addEventListener('click', () => {
+//         onProfile.classList.toggle("right__text--primary");
+//     });
+// });
+
+
+
